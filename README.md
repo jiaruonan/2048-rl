752f9fc2f7d1024144796e8ad4a87d2564f824d2
jiaruonan <jiaruonan97@gmail.com>
2022-08-01 16:14:04 +0800

    doc(jrn): add 2048 game env and test

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# 2048-rl
+# 2048 RL
+
+## 2048 
+
+[2048](https://play2048.co/) is a single-player sliding tile puzzle game. The objective of the game is to slide numbered tiles on a grid to combine them to create a tile with the number 2048(math: 2048 = 2**11).
+
+We use an open-source env of 2048 game, which is OpenAI Gym interface. Thanks for authors @dsgiitr, @rgal and so on.
+
+
+## RL Algorithms
+
+We are going to release some RL methods to play with 2048.
+
+
---
+++ env/test_env.py
@@ -0,0 +1,159 @@
+import numpy as np
+import game_env
+from game_env import Game2048Env
+import pytest
+
+class Test2048Logic():
+    def test_combine(self):
+        game = Game2048Env()
+        # Test not combining
+        assert game.combine([0, 0, 0, 0]) == ([0, 0, 0, 0], 0)
+        assert game.combine([2, 4, 8, 16]) == ([2, 4, 8, 16], 0)
+
+        # Test combining
+        # Left same same
+        assert game.combine([2, 2, 8, 0]) == ([4, 8, 0, 0], 4)
+        # Middle the same
+        assert game.combine([4, 2, 2, 4]) == ([4, 4, 4, 0], 4)
+        # Left and middle the same
+        assert game.combine([2, 2, 2, 8]) == ([4, 2, 8, 0], 4)
+        # Right the same
+        assert game.combine([2, 8, 4, 4]) == ([2, 8, 8, 0], 8)
+        # Left and right the same
+        assert game.combine([2, 2, 4, 4]) == ([4, 8, 0, 0], 12)
+        # Right and middle the same
+        assert game.combine([2, 4, 4, 4]) == ([2, 8, 4, 0], 8)
+        # All the same
+        assert game.combine([4, 4, 4, 4]) == ([8, 8, 0, 0], 16)
+
+        # Test short input
+        assert game.combine([]) == ([0, 0, 0, 0], 0)
+        assert game.combine([0]) == ([0, 0, 0, 0], 0)
+        assert game.combine([2]) == ([2, 0, 0, 0], 0)
+        assert game.combine([2, 4]) == ([2, 4, 0, 0], 0)
+        assert game.combine([2, 2, 8]) == ([4, 8, 0, 0], 4)
+
+    def test_shift(self):
+        game = Game2048Env()
+        # Shift left without combining
+        assert game.shift([0, 0, 0, 0], 0) == ([0, 0, 0, 0], 0)
+        assert game.shift([0, 2, 0, 0], 0) == ([2, 0, 0, 0], 0)
+        assert game.shift([0, 2, 0, 4], 0) == ([2, 4, 0, 0], 0)
+        assert game.shift([2, 4, 8, 16], 0) == ([2, 4, 8, 16], 0)
+
+        # Shift left and combine
+        assert game.shift([0, 2, 2, 8], 0) == ([4, 8, 0, 0], 4)
+        assert game.shift([2, 2, 2, 8], 0) == ([4, 2, 8, 0], 4)
+        assert game.shift([2, 2, 4, 4], 0) == ([4, 8, 0, 0], 12)
+
+        # Shift right without combining
+        assert game.shift([0, 0, 0, 0], 1) == ([0, 0, 0, 0], 0)
+        assert game.shift([0, 2, 0, 0], 1) == ([0, 0, 0, 2], 0)
+        assert game.shift([0, 2, 0, 4], 1) == ([0, 0, 2, 4], 0)
+        assert game.shift([2, 4, 8, 16], 1) == ([2, 4, 8, 16], 0)
+
+        # Shift right and combine
+        assert game.shift([2, 2, 8, 0], 1) == ([0, 0, 4, 8], 4)
+        assert game.shift([2, 2, 2, 8], 1) == ([0, 2, 4, 8], 4)
+        assert game.shift([2, 2, 4, 4], 1) == ([0, 0, 4, 8], 12)
+
+    def test_move(self):
+        # Test a bunch of lines all moving at once.
+        game = Game2048Env()
+        # Test shift up
+        game.set_board(np.array([
+            [0, 2, 0, 4],
+            [2, 2, 8, 0],
+            [2, 2, 2, 8],
+            [2, 2, 4, 4]]))
+        assert game.move(0) == 12
+        assert np.array_equal(game.get_board(), np.array([
+            [4, 4, 8, 4],
+            [2, 4, 2, 8],
+            [0, 0, 4, 4],
+            [0, 0, 0, 0]]))
+        # Test shift right
+        game.set_board(np.array([
+            [0, 2, 0, 4],
+            [2, 2, 8, 0],
+            [2, 2, 2, 8],
+            [2, 2, 4, 4]]))
+        assert game.move(1) == 20
+        assert np.array_equal(game.get_board(), np.array([
+            [0, 0, 2, 4],
+            [0, 0, 4, 8],
+            [0, 2, 4, 8],
+            [0, 0, 4, 8]]))
+        # Test shift down
+        game.set_board(np.array([
+            [0, 2, 0, 4],
+            [2, 2, 8, 0],
+            [2, 2, 2, 8],
+            [2, 2, 4, 4]]))
+        assert game.move(2) == 12
+        assert np.array_equal(game.get_board(), np.array([
+            [0, 0, 0, 0],
+            [0, 0, 8, 4],
+            [2, 4, 2, 8],
+            [4, 4, 4, 4]]))
+        # Test shift left
+        game.set_board(np.array([
+            [0, 2, 0, 4],
+            [2, 2, 8, 0],
+            [2, 2, 2, 8],
+            [2, 2, 4, 4]]))
+        assert game.move(3) == 20
+        assert np.array_equal(game.get_board(), np.array([
+            [2, 4, 0, 0],
+            [4, 8, 0, 0],
+            [4, 2, 8, 0],
+            [4, 8, 0, 0]]))
+
+        # Test that doing the same move again (without anything added) is illegal
+        with pytest.raises(game_env.IllegalMove):
+            game.move(3)
+
+        # Test a follow on move from the first one
+        assert game.move(2) == 8 # shift down
+        assert np.array_equal(game.get_board(), np.array([
+            [0, 4, 0, 0],
+            [2, 8, 0, 0],
+            [4, 2, 0, 0],
+            [8, 8, 8, 0]]))
+
+    def test_highest(self):
+        game = Game2048Env()
+        game.set_board(np.array([
+            [0, 2, 0, 4],
+            [2, 2, 8, 0],
+            [2, 2, 2048, 8],
+            [2, 2, 4, 4]]))
+        assert game.highest() == 2048
+
+    def test_isend(self):
+        game = Game2048Env()
+        game.set_board(np.array([
+            [2, 2, 2, 2],
+            [2, 2, 2, 2],
+            [2, 2, 2, 2],
+            [2, 2, 2, 2]]))
+        assert game.isend() == False
+        game.set_board(np.array([
+            [2, 4, 8, 16],
+            [4, 8, 16, 2],
+            [8, 16, 2, 4],
+            [16, 2, 4, 8]]))
+        assert game.isend() == True
+
+
+# # play with 2048Game
+# game = Game2048Env()
+# observation, reward, done, info = game.step(2)
+# # game.render()
+# game.step(1)
+# # game.render()
+# game.step(2)
+# # game.render()
+# game.step(1)
+# # game.render()
+# game.close()
